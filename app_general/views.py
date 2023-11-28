7a2404f289465c18046a877010d0eef2968c0f96
paucrisNaco <paucris37@gmail.com>
2023-11-28 15:15:45 -0300

    Estructura PW

---
+++ app_general/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+# Create your views here.
+def marcaPersonal(request):
+    return HttpResponse("<h1>Tecnología con propósito</h1>")
---
+++ app_trabaja_con_nosotros/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppTrabajaConNosotrosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_trabaja_con_nosotros'
